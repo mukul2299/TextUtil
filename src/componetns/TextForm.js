6be4ec40eaa436a7341154165270ce3b490be833
mukulpandit <mukul2299pandit@gmail.com>
2024-03-24 13:21:52 +0530

    Fixed few issues

--- src/componetns/TextForm.js
+++ src/componetns/TextForm.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react'
 
 export default function TextForm(prop) {
     const [enteredText, setText] = useState("");
-    const [noOfWords, setNoOfWords] = useState(0);
     function onUpperClick() {
         setText(enteredText.toUpperCase());
         prop.showAlert("converted to upper case.","success");
@@ -17,7 +16,6 @@ export default function TextForm(prop) {
     }
     function onTextChange(event) {
         setText(event.target.value);
-        setNoOfWords(enteredText.trim()===""? 0:enteredText.trim().split(" ").length)
     }
     return (
         <>
@@ -26,19 +24,19 @@ export default function TextForm(prop) {
                 <div className='mb-3' >
                     <textarea className="form-control" style={{ color: prop.theme === 'light' ? 'black' : 'white', backgroundColor: prop.theme === 'light' ? 'white' : 'black' }} onChange={onTextChange} value={enteredText} id="textArea" rows="6" />
                 </div>
-                <button className="btn btn-primary" onClick={onUpperClick}>Uppercase</button>
-                <button className="btn btn-primary mx-3" onClick={handleLowercase}>Lowercase</button>
-                <button className="btn btn-danger" onClick={handleClear}>Clear</button>
+                <button disabled={enteredText.length < 1} className="btn btn-primary" onClick={onUpperClick}>Uppercase</button>
+                <button disabled={enteredText.length < 1} className="btn btn-primary mx-3" onClick={handleLowercase}>Lowercase</button>
+                <button disabled={enteredText.length < 1} className="btn btn-danger" onClick={handleClear}>Clear</button>
 
             </div >
             <div className="container my-3" >
                 <h2 >Text Summary</h2>
-                <p>{noOfWords} words and {enteredText.trim().length} characters.</p>
-                <p>{noOfWords * 0.008} mins read.</p>
+                <p>{enteredText.trim().split(" ").filter((x)=>x!=="").length} words and {enteredText.trim().length} characters.</p>
+                <p>{enteredText.trim().split(" ").filter((x)=>x!=="").length * 0.008} mins read.</p>
             </div>
             <div className="container my-3">
                 <h2>Preview</h2>
-                <p >{enteredText.length < 1 ? 'Pease enter the text to preview.' : enteredText}</p>
+                <p >{enteredText.length < 1 ? 'Nothing to preview!' : enteredText}</p>
             </div>
         </>
     )
